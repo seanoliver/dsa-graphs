2f090a65b1a55d694d02ebe26c8a66f99f60bf96
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-01 14:03:06 -0700

    wip - distance of shortest path

--- graph.js
+++ graph.js
@@ -1,113 +1,147 @@
 /** Node class for graph. */
 
 class Node {
-  constructor(value, adjacent = new Set()) {
-    this.value = value;
-    this.adjacent = adjacent;
-  }
+	constructor(value, adjacent = new Set()) {
+		this.value = value;
+		this.adjacent = adjacent;
+	}
 }
-
 
 /** Graph class. */
 
 class Graph {
-  constructor() {
-    this.nodes = new Set();
-  }
+	constructor() {
+		this.nodes = new Set();
+	}
 
-  /** add Node instance and add it to nodes property on graph. */
-  addVertex(vertex) {
-    this.nodes.add(vertex);
-  }
+	/** add Node instance and add it to nodes property on graph. */
+	addVertex(vertex) {
+		this.nodes.add(vertex);
+	}
 
-  /** add array of new Node instances and adds to them to nodes property. */
-  addVertices(vertexArray) {
-    for (const vertex of vertexArray) {
-      this.nodes.add(vertex);
-    }
-  }
+	/** add array of new Node instances and adds to them to nodes property. */
+	addVertices(vertexArray) {
+		for (const vertex of vertexArray) {
+			this.nodes.add(vertex);
+		}
+	}
 
-  /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) {
-    v1.adjacent.add(v2);
-    v2.adjacent.add(v1);
-  }
+	/** add edge between vertices v1,v2 */
+	addEdge(v1, v2) {
+		v1.adjacent.add(v2);
+		v2.adjacent.add(v1);
+	}
 
-  /** remove edge between vertices v1,v2 */
-  removeEdge(v1, v2) {
-    v1.adjacent.delete(v2);
-    v2.adjacent.delete(v1);
-  }
+	/** remove edge between vertices v1,v2 */
+	removeEdge(v1, v2) {
+		v1.adjacent.delete(v2);
+		v2.adjacent.delete(v1);
+	}
 
-  /** remove vertex from graph:
-   *
-   * - remove it from nodes property of graph
-   * - update any adjacency lists using that vertex
-   */
-  removeVertex(vertex) {
-    // remove from all adjacency lists
-    for (const adj of vertex.adjacent) {
-      adj.adjacent.delete(vertex);
-    }
+	/** remove vertex from graph:
+	 *
+	 * - remove it from nodes property of graph
+	 * - update any adjacency lists using that vertex
+	 */
+	removeVertex(vertex) {
+		// remove from all adjacency lists
+		for (const adj of vertex.adjacent) {
+			adj.adjacent.delete(vertex);
+		}
 
-    // remove from nodes
-    this.nodes.delete(vertex);
+		// remove from nodes
+		this.nodes.delete(vertex);
+	}
 
-  }
+	/** traverse graph with DFS and returns array of Node values */
+	depthFirstSearch(start) {
+		// init a stack with 'start'
+		// init a set for nodesSeen
+		// base case: stack is empty
+		// base case: start.value === this.value
 
-  /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) {
-    // init a stack with 'start'
-    // init a set for nodesSeen
-    // base case: stack is empty
-    // base case: start.value === this.value
+		// remove 'start'
+		// add start to nodesSeen
+		// add start's adjacent nodes to stack
 
-    // remove 'start'
-    // add start to nodesSeen
-    // add start's adjacent nodes to stack
+		let searchStack = [start];
+		let visitedNodes = new Set();
 
-    let searchStack = [start];
-    let visitedNodes = new Set();
+		while (searchStack.length) {
+			let currentNode = searchStack.pop();
 
-    while (searchStack.length) {
-      let currentNode = searchStack.pop();
+			visitedNodes.add(currentNode.value);
 
-      visitedNodes.add(currentNode.value);
+			for (let adj of currentNode.adjacent) {
+				if (!visitedNodes.has(adj.value)) {
+					searchStack.push(adj);
+				}
+			}
+		}
 
-      for (let adj of currentNode.adjacent) {
-        if (!visitedNodes.has(adj.value)) {
-          searchStack.push(adj);
-        }
-      }
-    }
+		return [...visitedNodes];
+	}
 
-    return [...visitedNodes];
-  }
+	/** traverse graph with BDS and returns array of Node values */
+	breadthFirstSearch(start) {
+		let searchQueue = [start];
+		let visitedNodes = new Set();
 
-  /** traverse graph with BDS and returns array of Node values */
-  breadthFirstSearch(start) {
-    let searchQueue = [start];
-    let visitedNodes = new Set();
+		while (searchQueue.length) {
+			let currentNode = searchQueue.shift();
 
-    while (searchQueue.length) {
-      let currentNode = searchQueue.shift();
+			visitedNodes.add(currentNode.value);
 
-      visitedNodes.add(currentNode.value);
+			for (let adj of currentNode.adjacent) {
+				if (!visitedNodes.has(adj.value)) {
+					searchQueue.push(adj);
+				}
+			}
+		}
 
-      for (let adj of currentNode.adjacent) {
-        if (!visitedNodes.has(adj.value)) {
-          searchQueue.push(adj);
-        }
-      }
-    }
+		return [...visitedNodes];
+	}
 
-    return [...visitedNodes];
-  }
+	/** find the distance of the shortest path from the start vertex to the end vertex */
+	distanceOfShortestPath(start, end) {
 
-  /** find the distance of the shortest path from the start vertex to the end vertex */
-  distanceOfShortestPath(start, end) {
+		if (start.adjacent.has(end)) return 1;
+    if (start.adjacent.length === 0 || end.adjacent.length === 0) return undefined;
 
+
+    let distance = 0;
+		let distances = [];
+		for (const adjacentNode of start.adjacent) {
+      console.log('distance', distance);
+      console.log('adjacentNode', adjacentNode);
+
+      this.distanceOfShortestPath(adjacentNode, end);
+
+      // returned an array of distances
+		}
+    // return min of distances
+
+    return distance;
   }
 }
 
-module.exports = { Graph, Node }
+let graph = new Graph();
+
+let r = new Node('R');
+let i = new Node('I');
+let t = new Node('T');
+let h = new Node('H');
+let m = new Node('M');
+
+graph.addVertices([r, i, t, h, m]);
+
+graph.addEdge(r, i);
+graph.addEdge(r, t);
+graph.addEdge(r, h);
+graph.addEdge(i, t);
+graph.addEdge(t, h);
+graph.addEdge(h, m);
+
+console.log(graph.distanceOfShortestPath(r, m));
+
+module.exports = { Graph, Node };

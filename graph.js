44216af889fc86694498bc5e9995bb4b29210b73
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-06-01 11:59:30 -0700

    finished first few implemention methods and started pseudocode for DFS

--- graph.js
+++ graph.js
@@ -16,26 +16,57 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addVertex(vertex) { }
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addVertices(vertexArray) { }
+  addVertices(vertexArray) {
+    for (const vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
 
   /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) { }
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   /** remove edge between vertices v1,v2 */
-  removeEdge(v1, v2) { }
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   /** remove vertex from graph:
    *
    * - remove it from nodes property of graph
    * - update any adjacency lists using that vertex
    */
-  removeVertex(vertex) { }
+  removeVertex(vertex) {
+    // remove from all adjacency lists
+    for (const adj of vertex.adjacent) {
+      adj.adjacent.delete(vertex);
+    }
+
+    // remove from nodes
+    this.nodes.delete(vertex);
+
+  }
 
   /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) { }
+  depthFirstSearch(start) {
+    // init a stack with 'start'
+    // init a set for nodesSeen
+    // base case: stack is empty
+    // base case: start.value === this.value
+
+    // remove 'start'
+    // add start to nodesSeen
+    // add start's adjacent nodes to stack
+
+  }
 
   /** traverse graph with BDS and returns array of Node values */
   breadthFirstSearch(start) { }
